bc76e3babf558915e8081caecf4238db2e5b9f03
Nicola Montrone <nicola.montrone@thenemesis.io>
2023-10-15 17:32:34 +0200

    test

--- main.py
+++ main.py
@@ -1,80 +1,20 @@
-"""
-neuron poker
+from env import PokerEnv
+import argparse
+import test
 
-Usage:
-  main.py selfplay random [options]
-  main.py selfplay keypress [options]
-  main.py selfplay consider_equity [options]
-  main.py selfplay equity_improvement --improvement_rounds=<> [options]
-  main.py selfplay dqn_train [options]
-  main.py selfplay dqn_play [options]
-  main.py learn_table_scraping [options]
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--test", action="store_true", help="Run unit tests")
+    args = parser.parse_args()
 
-options:
-  -h --help                 Show this screen.
-  -r --render               render screen
-  -c --use_cpp_montecarlo   use cpp implementation of equity calculator. Requires cpp compiler but is 500x faster
-  -f --funds_plot           Plot funds at end of episode
-  --log                     log file
-  --name=<>                 Name of the saved model
-  --screenloglevel=<>       log level on screen
-  --episodes=<>             number of episodes to play
-  --stack=<>                starting stack for each player [default: 500].
-
-"""
-
-import logging
-from docopt import docopt
-from tools.helper import get_config
-from tools.helper import init_logger
-from classes.SelfPlay import SelfPlay
-# pylint: disable=import-outside-toplevel
-
-def command_line_parser():
-    args = docopt(__doc__)
-    logfile = args['--log'] if args['--log'] else 'default'
-    model_name = args['--name'] if args['--name'] else 'dqn1'
-    screenloglevel = args['--screenloglevel'] if args['--screenloglevel'] else 'INFO'
-    
-    _ = get_config()
-        
-    init_logger(screenlevel=screenloglevel, filename=logfile)
-    print(f"Screenloglevel: {screenloglevel}")
-    log = logging.getLogger("")
-    log.info("Initializing program")
-
-    if args['selfplay']:
-        num_episodes = 1 if not args['--episodes'] else int(args['--episodes'])
-        runner = SelfPlay(render=args['--render'], num_episodes=num_episodes,
-                          use_cpp_montecarlo=args['--use_cpp_montecarlo'],
-                          funds_plot=args['--funds_plot'],
-                          stack=int(args['--stack']))
-
-        if args['random']:
-            runner.random_agents()
-
-        elif args['keypress']:
-            runner.key_press_agents()
-
-        elif args['consider_equity']:
-            runner.equity_vs_random()
-
-        elif args['equity_improvement']:
-            improvement_rounds = int(args['--improvement_rounds'])
-            runner.equity_self_improvement(improvement_rounds)
-
-        elif args['dqn_train']:
-            runner.dqn_train_keras_rl(model_name)
-
-        elif args['dqn_play']:
-            runner.dqn_play_keras_rl(model_name)
-
-
+    if args.test:
+        test.run_tests()
     else:
-        raise RuntimeError("Argument not yet implemented")
+        env = PokerEnv(3, 500, 10, 20, None, False)
+        env.reset()
+        # Implementare la selezione delle azioni..
+        # while not env.done:
+        #   env.step()
 
-
-
-
-if __name__ == '__main__':
-    command_line_parser()
+if __name__ == "__main__":
+    main()
